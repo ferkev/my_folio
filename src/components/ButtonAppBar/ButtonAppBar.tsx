83051559886796dba4cc1a3a4498297f8fc60b7b
ferkev <ferkev@live.fr>
2024-06-13 10:42:12 +0200

    integrate materialui

---
+++ src/components/ButtonAppBar/ButtonAppBar.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { Button } from '@mui/material';
+
+export default function ButtonAppBar() {
+  const navItems = ['Home', 'About', 'Contact'];
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar component="nav">
+         <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'block', sm: 'block' } }}
+          >
+            MUI
+          </Typography>
+          <Box sx={{ display: { xs: 'block', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
